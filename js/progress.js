e7c6ad481bd2fe09565174ad38badba212320222
Anna Filou <38259055+anna-filou@users.noreply.github.com>
2023-01-10 19:50:53 +0200

    $('footer').outerHeight(true)

--- js/progress.js
+++ js/progress.js
@@ -1,6 +1,6 @@
 $(document).on('scroll', function() {
     var pixels = $(document).scrollTop()
-    var total = $(document).height() - $(window).height() - $('footer').outerHeight() - $('#more-articles').outerHeight() - 200
+    var total = $(document).height() - $(window).height() - $('footer').outerHeight(true) - $('#more-articles').outerHeight(true)
     var percentage = 100 * pixels / total 
     $('.progress').css('width', percentage + '%')
 })
