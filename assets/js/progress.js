8aa96425f811edfdd04a1cf34bc6f4591cedf611
Anna Filou <38259055+anna-filou@users.noreply.github.com>
2023-01-16 14:45:43 +0200

    fixed progress.js
    didn't work when #more-articles did not exist so i made a function to check and return 0 if it does not exist.

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,8 +1,14 @@
-$("h1").html("hello")
+function getHeight() {
+    if ($('#more-articles').length) {
+        return $('#more-articles').outerHeight(true);
+    } else { 
+        return 0;
+    }
+}
 
 $(document).on('scroll', function() {
     var pixels = $(document).scrollTop()
-    var total = $(document).height() - $(window).height() - $('footer').outerHeight(true) - $('#more-articles').outerHeight(true)
+    var total = $(document).height() - $(window).height() - $('footer').outerHeight(true) - getHeight()
     var percentage = 100 * pixels / total 
     $('.progress').css('width', percentage + '%')
 })
