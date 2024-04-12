f83708e49e44ddb60a5917f377fe8841a0852b83
Anna Filou <38259055+anna-filou@users.noreply.github.com>
2024-04-13 02:29:11 +0300

    removed jQuery dependency
    used gpt to convert progress bar script to vanilla js. commented out jQuery.

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,14 +1,30 @@
+// function getHeight() {
+//     if ($('#more-articles').length) {
+//         return $('#more-articles').outerHeight(true);
+//     } else { 
+//         return 0;
+//     }
+// }
+
+// $(document).on('scroll', function() {
+//     var pixels = $(document).scrollTop()
+//     var total = $(document).height() - $(window).height() - $('footer').outerHeight(true) - getHeight()
+//     var percentage = 100 * pixels / total 
+//     $('.progress').css('width', percentage + '%')
+// })
+
 function getHeight() {
-    if ($('#more-articles').length) {
-        return $('#more-articles').outerHeight(true);
+    var moreArticles = document.getElementById('more-articles');
+    if (moreArticles) {
+        return moreArticles.offsetHeight;
     } else { 
         return 0;
     }
 }
 
-$(document).on('scroll', function() {
-    var pixels = $(document).scrollTop()
-    var total = $(document).height() - $(window).height() - $('footer').outerHeight(true) - getHeight()
-    var percentage = 100 * pixels / total 
-    $('.progress').css('width', percentage + '%')
-})
+document.addEventListener('scroll', function() {
+    var pixels = window.scrollY || document.documentElement.scrollTop;
+    var total = document.documentElement.scrollHeight - window.innerHeight - document.querySelector('footer').offsetHeight - getHeight();
+    var percentage = 100 * pixels / total;
+    document.querySelector('.progress').style.width = percentage + '%';
+});
