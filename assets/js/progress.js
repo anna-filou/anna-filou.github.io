9082d2bf8c0bb91d9b063b8656d40822aa8705ad
Anna Filou <38259055+anna-filou@users.noreply.github.com>
2025-05-25 20:20:29 +0300

    add just text layout, modify progress.js to not need footer

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -7,9 +7,14 @@ function getHeight() {
     }
 }
 
+function getFooterHeight() {
+    var footer = document.querySelector('footer');
+    return footer ? footer.offsetHeight : 0;
+}
+
 document.addEventListener('scroll', function() {
     var pixels = window.scrollY || document.documentElement.scrollTop;
-    var total = document.documentElement.scrollHeight - window.innerHeight - document.querySelector('footer').offsetHeight - getHeight();
+    var total = document.documentElement.scrollHeight - window.innerHeight - getFooterHeight() - getHeight();
     var percentage = 100 * pixels / total;
     document.querySelector('.progress').style.width = percentage + '%';
 });
